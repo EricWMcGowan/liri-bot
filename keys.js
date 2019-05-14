280f58dad27c160ad24c4769b3a92c7ed892518a
Eric McGowan <ericw.mcgowan@gmail.com>
2019-05-13 19:16:18 -0500

    First Commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+
+
+exports.twitterKeys = {
+    consumer_key: 'key',
+    consumer_secret: 'key',
+    access_token_key: 'key',
+    access_token_secret: 'key',
+  }
+  
+  exports.tmdbKey = 'key';
